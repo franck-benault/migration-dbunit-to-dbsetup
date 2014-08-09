37c785ee47c3a11c504658905b0329f882d98d3d
franck-benault <franck-benault@orange.fr>
2014-08-09 23:21:54 +0200

    migration tool first analysis

--- src/main/java/net/franckbenault/migration/dbunittodbsetup/DBUnitToDbSetup.java
+++ src/main/java/net/franckbenault/migration/dbunittodbsetup/DBUnitToDbSetup.java
@@ -1,10 +1,20 @@
 package net.franckbenault.migration.dbunittodbsetup;
 
 public class DBUnitToDbSetup {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public void migration(String inputFolder, String outputFile) {
+		
+		
+		
+		//read folder list of xml files
+		
+		//for each xml file
+		//write operation
+			//delete part
+			//data part
+		
+		
+		FileUtils.writeToFile(outputFile, "texte");
 	}
 
 }
---
+++ src/main/java/net/franckbenault/migration/dbunittodbsetup/DBUnitToDbSetupMain.java
@@ -0,0 +1,15 @@
+package net.franckbenault.migration.dbunittodbsetup;
+
+public class DBUnitToDbSetupMain {
+
+	public static void main(String[] args) {
+
+
+		//Input folder to reader the xml file
+		
+		
+		//output file with DBSetup java code
+
+	}
+
+}
