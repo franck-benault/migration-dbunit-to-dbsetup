f32bda0d43316b85fc32a90290f7ef8c2b9e676b
franck-benault <franck-benault@orange.fr>
2014-08-10 13:58:38 +0200

    complete output file

--- src/main/java/net/franckbenault/migration/dbunittodbsetup/DBUnitToDbSetup.java
+++ src/main/java/net/franckbenault/migration/dbunittodbsetup/DBUnitToDbSetup.java
@@ -1,5 +1,8 @@
 package net.franckbenault.migration.dbunittodbsetup;
 
+import java.io.File;
+import java.util.List;
+
 public class DBUnitToDbSetup {
 	
 	/**
@@ -10,17 +13,23 @@ public class DBUnitToDbSetup {
 	 */
 	public int migration(String inputFolder, String outputFile) {
 		
-		
+		//HEADER
+		String header = "public class DBSetupCommonOperations {\n";
+		FileUtils.writeToFile(outputFile, header, false);
 		
 		//read folder list of xml files
+	    String headerOperation = "public static final Operation insertVendorsAndProducts =\n sequenceOf(\n";
+		List<File> files = FileUtils.listOfFiles("./src/test/resources");
+		
+		for(File file : files) {
+			FileUtils.writeToFile(outputFile, headerOperation, true);			
+		}
 		
 		//for each xml file
 		//write operation
 			//delete part
 			//data part
-		
-		
-		FileUtils.writeToFile(outputFile, "texte", false);
+	
 		return 0;
 	}
 
